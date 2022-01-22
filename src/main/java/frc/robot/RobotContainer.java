29134d754cefde5fb0c570c0ec2591b4b9fe874c
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2022-01-22 12:38:10 +0200

    added auto selector

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,20 +4,12 @@
 
 package frc.robot;
 
-import java.util.List;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.Constants.Drivetrain.SwerveModuleConstants;
-import frc.robot.commands.FollowTrajectory;
+import frc.robot.autonomous.AutoSelector;
 import frc.robot.humanIO.Joysticks;
 import frc.robot.humanIO.PS5Controller.Button;
 import frc.robot.subsystems.drivetrain.Drivetrain;
@@ -43,6 +35,8 @@ public class RobotContainer {
      * The container for the robot. Contains subsystems, OI devices, and commands.
      */
     public RobotContainer() {
+        AutoSelector.initialize(m_Drivetrain);
+
         // Configure the button bindings
         configureButtonBindings();
         m_Drivetrain.setDefaultCommand(
@@ -78,24 +72,7 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
-        return new FollowTrajectory(m_Drivetrain, getTrajectory());
+        return AutoSelector.get("Test Auto");
     }
 
-    private static Trajectory getTrajectory() {
-        TrajectoryConfig config = new TrajectoryConfig(Constants.Autonomous.kMaxSpeedMetersPerSecond,
-                Constants.Autonomous.kMaxAccelerationMetersPerSecondSquared)
-                        // Add kinematics to ensure max speed is actually obeyed
-                        .setKinematics(Constants.Drivetrain.kinematics);
-
-        // An example trajectory to follow. All units in meters.
-        return TrajectoryGenerator.generateTrajectory(
-                // Start at the origin facing the +X direction
-                new Pose2d(0, 0, new Rotation2d(0)),
-                // Pass through these two interior waypoints, making an 's' curve path
-                List.of(new Translation2d(1, 0.5), new Translation2d(2, -0.5)),
-                // List.of(),
-                // End 3 meters straight ahead of where we started, facing forward
-                new Pose2d(3, 0, Rotation2d.fromDegrees(0)), config);
-
-    }
 }
