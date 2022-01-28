053a8638eabe393d013b77fc4749436ac99b3a58
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2022-01-28 17:52:01 +0200

    unified follower and command

--- src/main/java/frc/robot/commands/FollowTrajectory.java
+++ src/main/java/frc/robot/commands/FollowTrajectory.java
@@ -4,65 +4,94 @@
 
 package frc.robot.commands;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.autonomous.SwerveTrajectoryFollower;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
 public class FollowTrajectory extends CommandBase {
+    private final Timer _timer = new Timer();
+    private HolonomicDriveController _controller;
+    private SwerveDriveKinematics _kinematics;
+    private Trajectory _trajectory;
+    private Consumer<SwerveModuleState[]> _outputModuleStates;
+    private Supplier<Pose2d> _pose;
+    private Rotation2d _angleRef;
+    private final Drivetrain _drivetrain;
 
-  private Drivetrain _drivetrain;
-  private SwerveTrajectoryFollower _follower;
-  private Trajectory _trajectory;
+    /** Creates a new FollowTrajectoryTemp. */
+    public FollowTrajectory(Drivetrain drivetrain, Trajectory trajectory) {
+        this._drivetrain = drivetrain;
+        this._kinematics = Constants.Drivetrain.kinematics;
+        this._trajectory = trajectory;
+        _pose = this._drivetrain::getPose;
+        _outputModuleStates = this._drivetrain::setDesiredStates;
+        _angleRef = _trajectory.getStates().get(_trajectory.getStates().size() - 1).poseMeters.getRotation();
 
-  /** Creates a new FollowTrajectory. */
-  public FollowTrajectory(Drivetrain drivetrain, Trajectory trajectory) {
-    _drivetrain = drivetrain;
-    _trajectory = trajectory;
-    _follower = getFollower(_trajectory);
-    addRequirements(_drivetrain);
-  }
+        var thetaController = new ProfiledPIDController(Constants.Autonomous.kPThetaController, 0, 0,
+                Constants.Autonomous.kThetaControllerConstraints);
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    _drivetrain.resetOdometry(_trajectory.getInitialPose());
-    _follower.initialize();
-  }
+        _controller = new HolonomicDriveController(
+                new PIDController(Constants.Autonomous.kPXController, 0, 0),
+                new PIDController(Constants.Autonomous.kPYController, 0, 0),
+                thetaController);
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    _follower.execute();
-  }
+        addRequirements(this._drivetrain);
+    }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    _drivetrain.drive(0, 0, 0, false);
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        _drivetrain.resetOdometry(_trajectory.getInitialPose());
+        _timer.reset();
+        _timer.start();
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return _follower.isFinished();
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        double curTime = _timer.get();
+        var desiredState = _trajectory.sample(curTime);
+        var pose = _pose.get();
 
-  private SwerveTrajectoryFollower getFollower(Trajectory trajectory) {
+        var targetChassisSpeeds = _controller.calculate(pose, desiredState, _angleRef);
+        targetChassisSpeeds.omegaRadiansPerSecond *= -1;
+        System.out.printf("%f %f %f %f %f %f %f %f %f\n",
+                pose.getX(),
+                desiredState.poseMeters.getX(),
+                pose.getY(),
+                desiredState.poseMeters.getY(),
+                pose.getRotation().getDegrees(),
+                _angleRef.getDegrees(),
+                Math.hypot(targetChassisSpeeds.vxMetersPerSecond, targetChassisSpeeds.vyMetersPerSecond),
+                desiredState.velocityMetersPerSecond);
 
-    var thetaController = new ProfiledPIDController(Constants.Autonomous.kPThetaController, 0, 0,
-        Constants.Autonomous.kThetaControllerConstraints);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+        var targetModuleStates = _kinematics.toSwerveModuleStates(targetChassisSpeeds);
 
-    SwerveTrajectoryFollower swerveTrajectoryFollower = new SwerveTrajectoryFollower(trajectory,
-        _drivetrain::getPose, Constants.Drivetrain.kinematics,
-        new PIDController(Constants.Autonomous.kPXController, 0, 0),
-        new PIDController(Constants.Autonomous.kPYController, 0, 0), thetaController, _drivetrain::setDesiredStates);
+        _outputModuleStates.accept(targetModuleStates);
+    }
 
-    return swerveTrajectoryFollower;
-  }
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        _drivetrain.drive(0, 0, 0, false);
+    }
 
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return _timer.hasElapsed(_trajectory.getTotalTimeSeconds());
+    }
 }
