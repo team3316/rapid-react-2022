d6471cbf0e472aa35eb0f6381172361305ebf61f
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2022-01-28 17:25:55 +0200

    updated constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import frc.robot.motors.PIDFGains;
+import frc.robot.motors.units.UnitConversions;
+import frc.robot.motors.units.VelocityUnit;
 
 /**
  * Constants
@@ -12,13 +14,14 @@ public class Constants {
 
     public static final class Drivetrain {
         public static class SwerveModuleConstants {
-            public static final double freeSpeedMetersPerSecond = 3.6576;
+            private static final double neoMaxSpeed = 5600;
             public static final double driveRatio = 1.0 / 8.14;
             public static final double steeringRatio = 1.0 / 12.8;
             public static final double wheelDiameterMeters = 4 * 2.54 / 100; // 4 inches in meters
-            // public static final double wheelCircumferenceMeters = wheelRadiusMeters * 2 * Math.PI;
-            // public static final double driveDPRMeters = wheelCircumferenceMeters * driveRatio;
-
+            public static final double freeSpeedMetersPerSecond = neoMaxSpeed * 
+                new UnitConversions(driveRatio, wheelDiameterMeters)
+                    .getRPMModifier(VelocityUnit.MetersPerSecond); // 3.6598
+            
             public final Translation2d position;
             public final int idDrive;
             public final PIDFGains driveGains;
@@ -28,7 +31,7 @@ public class Constants {
             public final int canCoderId;
 
             public SwerveModuleConstants(Translation2d position, int idDrive, int idSteering, double cancoderZeroAngle,int canCoderId) {
-                this(position, idDrive, idSteering, new PIDFGains(0.0002 * 60/ (driveRatio * wheelDiameterMeters * Math.PI), 0, 0.01 * 60/ (driveRatio * wheelDiameterMeters * Math.PI), 1.0/6/902.0 * 0.95 * 60 / (driveRatio * wheelDiameterMeters * Math.PI), 50, 0), new PIDFGains(0.35 / steeringRatio, 0.0, 0, 0, 1, 0), cancoderZeroAngle, canCoderId);
+                this(position, idDrive, idSteering, new PIDFGains(0.0002, 0, 0.01, 0.0001848, 0, 0), new PIDFGains(0.35, 0.0, 0, 0, 0, 0), cancoderZeroAngle, canCoderId);
             }
 
             public SwerveModuleConstants(Translation2d position, int idDrive, int idSteering, PIDFGains driveGains, PIDFGains steeringGains, double cancoderZeroAngle, int canCoderId) {
@@ -42,10 +45,13 @@ public class Constants {
             }
         }
 
-        public static final SwerveModuleConstants TLModule = new SwerveModuleConstants(new Translation2d(-0.215, 0.215), 3, 4, 188.8, 11);
-        public static final SwerveModuleConstants TRModule = new SwerveModuleConstants(new Translation2d(0.215, 0.215), 1, 2, 122.3, 10);
-        public static final SwerveModuleConstants BLModule = new SwerveModuleConstants(new Translation2d(-0.215, -0.215), 5, 6, 290.4, 12);
-        public static final SwerveModuleConstants BRModule = new SwerveModuleConstants(new Translation2d(0.215, -0.215), 7, 8, 143.0, 13);
+        private static final double frontWheelDistMeters = 0.6703;
+        private static final double sideWheelDistMeters = 0.5102;
+
+        public static final SwerveModuleConstants TLModule = new SwerveModuleConstants(new Translation2d(-sideWheelDistMeters/2, frontWheelDistMeters/2), 3, 4, 188.8, 11);
+        public static final SwerveModuleConstants TRModule = new SwerveModuleConstants(new Translation2d(sideWheelDistMeters/2, frontWheelDistMeters/2), 1, 2, 122.3, 10);
+        public static final SwerveModuleConstants BLModule = new SwerveModuleConstants(new Translation2d(-sideWheelDistMeters/2, -frontWheelDistMeters/2), 5, 6, 290.4, 12);
+        public static final SwerveModuleConstants BRModule = new SwerveModuleConstants(new Translation2d(sideWheelDistMeters/2, -frontWheelDistMeters/2), 7, 8, 143.0, 13);
 
         public static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(TRModule.position,
                 TLModule.position, BRModule.position, BLModule.position);
