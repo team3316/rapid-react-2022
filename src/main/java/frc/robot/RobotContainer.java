8dc614fd3499c67b9704ace9ef2f2aa02cea6b67
Mika <mikush.carmel@gmail.com>
2022-02-03 18:53:45 +0200

    fixed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,26 +6,20 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
-<<<<<<< HEAD
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.Constants.Drivetrain.SwerveModuleConstants;
 import frc.robot.autonomous.AutoSelector;
-import frc.robot.humanIO.Joysticks;
-import frc.robot.humanIO.PS5Controller.Button;
-import frc.robot.subsystems.drivetrain.Drivetrain;
-=======
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.commands.Shoot;
 import frc.robot.humanIO.Joysticks;
+import frc.robot.humanIO.PS5Controller.Button;
 import frc.robot.subsystems.Trigger;
 import frc.robot.subsystems.Trigger.Side;
 import frc.robot.subsystems.Trigger.TriggerState;
+import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.manipulator.Manipulator;
 import frc.robot.subsystems.manipulator.Manipulator.ManipulatorState;
->>>>>>> intake
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -37,11 +31,14 @@ import frc.robot.subsystems.manipulator.Manipulator.ManipulatorState;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-<<<<<<< HEAD
     // The robot's subsystems and commands are defined here...
     private final Drivetrain m_Drivetrain = new Drivetrain();
 
+    private final Manipulator m_manipulator = new Manipulator();
+
     private final Joysticks m_Joysticks = new Joysticks();
+
+    private final Trigger m_trigger = new Trigger();
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -71,6 +68,20 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
         m_Joysticks.getButton(Button.kShare).whenPressed(() -> m_Drivetrain.resetYaw());
+
+        m_Joysticks.getButton(Button.kL1)
+        .toggleWhenPressed(
+          new StartEndCommand(
+            () -> m_manipulator.setState(ManipulatorState.COLLECT),
+            () -> m_manipulator.setState(ManipulatorState.OFF), 
+            m_manipulator)
+          .withInterrupt(() -> m_manipulator.getCargoState().hasBoth()));
+      
+      m_Joysticks.getButton(Button.kR1)
+        .whileHeld(new Shoot(m_manipulator));
+  
+      m_Joysticks.getButton(Button.kCross).whenHeld(this.getSetTriggerState(Side.LEFT));
+      m_Joysticks.getButton(Button.kCircle).whenHeld(this.getSetTriggerState(Side.RIGHT));
     }
 
     /**
@@ -83,55 +94,8 @@ public class RobotContainer {
         return AutoSelector.get("Test Auto");
     }
 
-=======
-  // The robot's subsystems and commands are defined here...
-  private final Manipulator m_manipulator = new Manipulator();
-
-  private final Joysticks m_joysticks = new Joysticks();
-  private final Trigger m_trigger = new Trigger();
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
-  }
-
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    m_joysticks.getButton(Button.kLeftBumper)
-      .toggleWhenPressed(
-        new StartEndCommand(
-          () -> m_manipulator.setState(ManipulatorState.COLLECT),
-          () -> m_manipulator.setState(ManipulatorState.OFF), 
-          m_manipulator)
-        .withInterrupt(() -> m_manipulator.getCargoState().hasBoth()));
-    
-    m_joysticks.getButton(Button.kRightBumper)
-      .whileHeld(new Shoot(m_manipulator));
-
-    m_joysticks.getButton(Button.kA).whenHeld(this.getSetTriggerState(Side.LEFT));
-    m_joysticks.getButton(Button.kB).whenHeld(this.getSetTriggerState(Side.RIGHT));
-
-  }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return new InstantCommand();
-  }
-  
-
-  public Command getSetTriggerState(Side side){
-    // TODO check if needed to end the command in another way
-    return new StartEndCommand(() -> m_trigger.setState(TriggerState.OUT, side), () -> m_trigger.setState(TriggerState.IN, side));
-  }
->>>>>>> intake
+    public Command getSetTriggerState(Side side){
+      // TODO check if needed to end the command in another way
+      return new StartEndCommand(() -> m_trigger.setState(TriggerState.OUT, side), () -> m_trigger.setState(TriggerState.IN, side));
+    }
 }
