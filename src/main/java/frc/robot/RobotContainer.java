c2af658e270ea1b1a084864e1721cb88cb696faa
shelef <shelef@guardicore.com>
2022-02-07 10:09:31 +0200

    Move arm into subsystems

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.Constants.ArmConstants;
-import frc.robot.Arm.subsystems.Arm;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.humanIO.Joysticks;
 import frc.robot.humanIO.PS5Controller.Button;
