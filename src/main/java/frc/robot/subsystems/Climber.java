faf3d72ee622745996fcdef69dbd7c8f94f4fec2
Mika <mikush.carmel@gmail.com>
2022-02-11 18:15:15 +0200

    changed PID to 2 slots + formatting

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax.IdleMode;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,72 +17,80 @@ import frc.robot.motors.units.PositionUnit;
 import frc.robot.motors.units.UnitConversions;
 
 public class Climber extends SubsystemBase {
-  
-  private DBugSparkMax _leftSparkMax, _rightSparkMax;
 
-  private double _lastPosition;
+    private DBugSparkMax _leftSparkMax, _rightSparkMax;
 
-  public Climber() {
-    this._leftSparkMax = new DBugSparkMax(Constants.Climber.deviceNumberLeft, new UnitConversions(Constants.Climber.gearRatio));
-    this._rightSparkMax = new DBugSparkMax(Constants.Climber.deviceNumberRight, new UnitConversions(Constants.Climber.gearRatio));
+    private double _lastPosition;
 
-    this._leftSparkMax.restoreFactoryDefaults();
-    this._rightSparkMax.restoreFactoryDefaults();
+    public Climber() {
+        this._leftSparkMax = new DBugSparkMax(Constants.Climber.leftID,
+                new UnitConversions(Constants.Climber.gearRatio));
+        this._rightSparkMax = new DBugSparkMax(Constants.Climber.rightID,
+                new UnitConversions(Constants.Climber.gearRatio));
 
-    this._rightSparkMax.follow(this._leftSparkMax, Constants.Climber.invert);
+        this._leftSparkMax.restoreFactoryDefaults();
+        this._rightSparkMax.restoreFactoryDefaults();
 
-    this._leftSparkMax.setupPIDF(new PIDFGains(Constants.Climber.kP, 0, 0, Constants.Climber.kF, 0, 0));
+        this._leftSparkMax.setIdleMode(IdleMode.kBrake);
+        this._rightSparkMax.setIdleMode(IdleMode.kBrake);
 
-    this._leftSparkMax.getEncoder().setPositionConversionFactor(Constants.Climber.conversionFactor);
+        this._rightSparkMax.follow(this._leftSparkMax, true);
 
-    initSDB();
-  }
+        this._leftSparkMax.setupPIDF(new PIDFGains(Constants.Climber.kP_slot0, 0, 0, Constants.Climber.kF_slot0, 0, 0));
+        this._leftSparkMax.getPIDController().setP(Constants.Climber.kP_slot1, Constants.Climber.PIDslot);
+        this._leftSparkMax.getPIDController().setI(0, Constants.Climber.PIDslot);
+        this._leftSparkMax.getPIDController().setD(0, Constants.Climber.PIDslot);
+        this._leftSparkMax.getPIDController().setFF(Constants.Climber.kF_slot1, Constants.Climber.PIDslot);
+        this._leftSparkMax.getPIDController().setOutputRange(-1.0, 1.0, Constants.Climber.PIDslot);
 
-  public enum ClimberState{
-    // TODO: add real values
-    UP(0.0), DOWN(0.0);
+        this._leftSparkMax.getEncoder().setPositionConversionFactor(Constants.Climber.conversionFactor);
 
-    public final double position;
-
-    ClimberState(double position){
-      this.position = position;
+        initSDB();
     }
-  }
 
-  public void setPosition(ClimberState state) {
-    this._lastPosition = state.position;
-    this._leftSparkMax.set(ControlMode.Position, state.position);
-  }
+    public enum ClimberState {
+        // TODO: add real values
+        UP(0.0), DOWN(0.0);
 
-  public boolean isLastPositionDown(){
-      return (this._lastPosition == ClimberState.DOWN.position);
-  }
+        public final double position;
 
-  private double getPosition(){
-    return this._leftSparkMax.getPosition(PositionUnit.Rotations);
-  }
+        ClimberState(double position) {
+            this.position = position;
+        }
+    }
 
-  private void initSDB(){
-    SmartDashboard.putNumber("kP", Constants.Climber.kP);
-    SmartDashboard.putNumber("kF", Constants.Climber.kF);
+    public void setPosition(ClimberState state) {
+        this._lastPosition = state.position;
+        this._leftSparkMax.set(ControlMode.Position, state.position);
+    }
 
-    SmartDashboard.putData("update pidf", new InstantCommand(() -> updatePIDF()));
-  }
+    public boolean isLastPositionDown() {
+        return (this._lastPosition == ClimberState.DOWN.position);
+    }
 
-  private void updateSDB(){
-    SmartDashboard.putNumber("kP", SmartDashboard.getNumber("kP", Constants.Climber.kP));
-    SmartDashboard.putNumber("kF", SmartDashboard.getNumber("kF", Constants.Climber.kF));
+    private double getPosition() {
+        return this._leftSparkMax.getPosition(PositionUnit.Rotations);
+    }
 
-    SmartDashboard.putNumber("Climber Position", getPosition());
-  }
+    private void initSDB() {
+        SmartDashboard.setDefaultNumber("kP loaded behavior", Constants.Climber.kP_slot0);
+        SmartDashboard.setDefaultNumber("kF loaded behavior", Constants.Climber.kF_slot0);
 
-  private void updatePIDF(){
-    this._leftSparkMax.setupPIDF(new PIDFGains(SmartDashboard.getNumber("kP", Constants.Climber.kP), 0, 0, SmartDashboard.getNumber("kF", Constants.Climber.kF), 0, 0));
-  }
+        SmartDashboard.putData("update pidf", new InstantCommand(() -> updatePIDF()));
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-    updateSDB();
-  }
+    private void updateSDB() {
+        SmartDashboard.putNumber("Climber Position", getPosition());
+    }
+
+    private void updatePIDF() {
+        this._leftSparkMax.setupPIDF(new PIDFGains(SmartDashboard.getNumber("kP", Constants.Climber.kP_slot0), 0, 0,
+                SmartDashboard.getNumber("kF", Constants.Climber.kF_slot0), 0, 0));
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        updateSDB();
+    }
 }
