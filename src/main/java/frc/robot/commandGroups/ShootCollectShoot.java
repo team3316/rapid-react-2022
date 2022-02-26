2b4cb29d0de2d049ab0a969d071555d237a83b8f
Mika <mikush.carmel@gmail.com>
2022-02-26 14:58:23 +0200

    fixed command groups

--- src/main/java/frc/robot/commandGroups/ShootCollectShoot.java
+++ src/main/java/frc/robot/commandGroups/ShootCollectShoot.java
@@ -7,20 +7,27 @@ package frc.robot.commandGroups;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
+import frc.robot.autonomous.FollowTrajectory;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.manipulator.Manipulator;
+import frc.robot.subsystems.manipulator.Manipulator.ManipulatorState;
 import frc.robot.subsystems.trigger.Trigger;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ShootCollect extends SequentialCommandGroup {
+public class ShootCollectShoot extends SequentialCommandGroup {
   /** Creates a new ShootCollect. */
-  public ShootCollect(Manipulator manipulator, Trigger trigger, Arm arm) {
+  public ShootCollectShoot(Manipulator manipulator, Trigger trigger, Arm arm, FollowTrajectory followTrajectory, FollowTrajectory followTrajectoryReverse) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new InstantCommand(() -> arm.setActiveGoal(Constants.ArmConstants.shootAngle)),
+    addCommands(arm.getActiveGoalCommand(Constants.ArmConstants.shootAngle),
                 new AutoShoot(manipulator, trigger), 
-                new InstantCommand(() -> arm.setActiveGoal(Constants.ArmConstants.startingAngle)));
+                arm.getActiveGoalCommand(Constants.ArmConstants.startingAngle),
+                raceWith(new InstantCommand(() -> manipulator.setState(ManipulatorState.COLLECT)),
+                        followTrajectory.getFollowTrajectoryCommand()),
+                followTrajectoryReverse.getFollowTrajectoryCommand(),
+                arm.getActiveGoalCommand(Constants.ArmConstants.shootAngle),
+                new AutoShoot(manipulator, trigger));
   }
 }
