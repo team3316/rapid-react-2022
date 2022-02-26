7a4b7ef7c9925c90b2fdceec246dcb13250c519b
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2022-02-26 13:55:02 +0200

    set desired rotation to last state rotation

--- src/main/java/frc/robot/autonomous/FollowTrajectory.java
+++ src/main/java/frc/robot/autonomous/FollowTrajectory.java
@@ -65,7 +65,8 @@ public class FollowTrajectory extends SubsystemBase {
 
         SmartDashboard.putData("Update Auto PID", new InstantCommand(() -> updatePID()));
         SmartDashboard.putData("Drive 2M in X", new InstantCommand(() -> getCalibrateXControllerCommand().schedule()));
-        SmartDashboard.putData("Rotate 180 in Theta", new InstantCommand( () -> getCalibrateThetaControllerCommand().schedule()));
+        SmartDashboard.putData("Rotate 180 in Theta",
+                new InstantCommand(() -> getCalibrateThetaControllerCommand().schedule()));
         SmartDashboard.putData("Update Auto Trajectory", new InstantCommand(() -> updateTrajectory()));
 
         this._chooser = new SendableChooser<String>();
@@ -78,31 +79,35 @@ public class FollowTrajectory extends SubsystemBase {
         SmartDashboard.putData("paths chooser", this._chooser);
     }
 
-    public void periodic(){
+    public void periodic() {
         SmartDashboard.putNumber("x pose", m_drivetrain.getPose().getX());
     }
 
     private void updatePID() {
         _xController.setP(SmartDashboard.getNumber("P Gain Auto xController", Constants.Autonomous.kPXController));
         _yController.setP(SmartDashboard.getNumber("P Gain Auto yController", Constants.Autonomous.kPYController));
-        _thetaController.setP(SmartDashboard.getNumber("P Gain Auto thetaCotroller", Constants.Autonomous.kPThetaController));
+        _thetaController
+                .setP(SmartDashboard.getNumber("P Gain Auto thetaCotroller", Constants.Autonomous.kPThetaController));
     }
 
     private Command getCalibrateXControllerCommand() {
         return new PIDCommand(this._xController,
                 () -> m_drivetrain.getPose().getX(),
                 2.0,
-                (double value) -> m_drivetrain.drive(value, 0, 0, false)).beforeStarting(() -> m_drivetrain.resetOdometry(new Pose2d()));
+                (double value) -> m_drivetrain.drive(value, 0, 0, false))
+                        .beforeStarting(() -> m_drivetrain.resetOdometry(new Pose2d()));
     }
 
     private Command getCalibrateThetaControllerCommand() {
-        ProfiledPIDCommand command =  new ProfiledPIDCommand(this._thetaController,
-        () -> m_drivetrain.getRotation2d().getRadians(),
-        Math.PI + m_drivetrain.getRotation2d().getRadians(),
-        (Double value, State state) -> {m_drivetrain.drive(0, 0, value, false);
-             SmartDashboard.putNumber("output", value);
-             SmartDashboard.putNumber("wanted pos", state.position); });
-            
+        ProfiledPIDCommand command = new ProfiledPIDCommand(this._thetaController,
+                () -> m_drivetrain.getRotation2d().getRadians(),
+                Math.PI + m_drivetrain.getRotation2d().getRadians(),
+                (Double value, State state) -> {
+                    m_drivetrain.drive(0, 0, value, false);
+                    SmartDashboard.putNumber("output", value);
+                    SmartDashboard.putNumber("wanted pos", state.position);
+                });
+
         command.getController().enableContinuousInput(-Math.PI, Math.PI);
         return command;
     }
@@ -110,17 +115,24 @@ public class FollowTrajectory extends SubsystemBase {
     private void updateTrajectory() {
         this.m_trajectory = PathPlanner.loadPath(this._chooser.getSelected(),
                 SmartDashboard.getNumber("Auto max velocity", Constants.Autonomous.kMaxSpeedMetersPerSecond),
-                SmartDashboard.getNumber("Auto Max Accleration", Constants.Autonomous.kMaxAccelerationMetersPerSecondSquared));
+                SmartDashboard.getNumber("Auto Max Accleration",
+                        Constants.Autonomous.kMaxAccelerationMetersPerSecondSquared));
     }
 
     public Command getFollowTrajectoryCommand() {
-        // return new PPSwerveControllerCommand(m_trajectory, _pose, _kinematics, _xController, _yController,
-        //         _thetaController, _outputModuleStates, m_drivetrain);
+        // return new PPSwerveControllerCommand(m_trajectory, _pose, _kinematics,
+        // _xController, _yController,
+        // _thetaController, _outputModuleStates, m_drivetrain);
 
         PathPlannerState initState = m_trajectory.getInitialState();
 
-        return new DBugPPSwerveControllerCommand(m_trajectory, _pose, _kinematics, _xController, _yController,
-                _thetaController, _outputModuleStates, m_drivetrain)
+        return new DBugPPSwerveControllerCommand(m_trajectory,
+                _pose, _kinematics,
+                _xController, _yController,
+                _thetaController,
+                _outputModuleStates,
+                () -> m_trajectory.getEndState().holonomicRotation,
+                m_drivetrain)
                         .beforeStarting(() -> m_drivetrain.resetOdometry(
                                 new Pose2d(initState.poseMeters.getTranslation(), initState.holonomicRotation)));
     }
