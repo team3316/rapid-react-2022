4df1fd639864cbeb11681364ae5584eb1e92a872
Mika <mikush.carmel@gmail.com>
2022-03-01 20:35:10 +0200

    fixed collect command + constants

--- src/main/java/frc/robot/commands/Collect.java
+++ src/main/java/frc/robot/commands/Collect.java
@@ -38,6 +38,6 @@ public class Collect extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return this._manipulator.getCargoState().hasBoth();
     }
 }
