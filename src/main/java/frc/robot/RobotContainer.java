25032fb0541887840a5c0ebcf6e30514ab49b597
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2022-03-12 12:03:54 +0200

    removed redundant code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -148,10 +148,7 @@ public class RobotContainer {
 
         m_Joysticks.getOperatorButton(Button.kTriangle).whenPressed(
                 new ConditionalCommand(
-                        new InstantCommand(
-                                () -> m_arm.getActiveGoalCommand(ArmConstants.shootAngle)
-                                        .andThen(() -> m_arm.setPercent(-0.1))
-                                        .schedule()),
+                        new InstantCommand(() -> m_arm.getActiveGoalCommand(ArmConstants.shootAngle).schedule()),
                         new InstantCommand(() -> m_arm.getActiveGoalCommand(ArmConstants.intakeAngle).schedule()),
                         m_arm::isLastGoalIntake)
                                 .beforeStarting(new InstantCommand(
