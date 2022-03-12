c5fb9e61b59e25245a3a6c037f7cfefb6d2cedf3
shelef <shelef@guardicore.com>
2022-03-12 10:59:51 +0200

    Clean up follow trajectory stuff

--- src/main/java/frc/robot/autonomous/FollowTrajectory.java
+++ src/main/java/frc/robot/autonomous/FollowTrajectory.java
@@ -16,15 +16,12 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 
-public class FollowTrajectory extends SubsystemBase {
+public class FollowTrajectory {
 
     private final Drivetrain m_drivetrain;
     private PathPlannerTrajectory m_trajectory;
@@ -34,8 +31,6 @@ public class FollowTrajectory extends SubsystemBase {
     private PIDController _yController;
     private ProfiledPIDController _thetaController;
     private Consumer<SwerveModuleState[]> _outputModuleStates;
-
-    private SendableChooser<String> _chooser;
 
     public FollowTrajectory(Drivetrain drivetrain, String path) {
         this.m_drivetrain = drivetrain;
@@ -50,47 +45,6 @@ public class FollowTrajectory extends SubsystemBase {
                 Constants.Autonomous.kThetaControllerConstraints);
         _thetaController.enableContinuousInput(-Math.PI, Math.PI);
         this._outputModuleStates = this.m_drivetrain::setDesiredStates;
-        // initSDB();
-    }
-
-    private void initSDB() {
-        SmartDashboard.setDefaultNumber("P Gain Auto xController", Constants.Autonomous.kPXController);
-        SmartDashboard.setDefaultNumber("P Gain Auto yController", Constants.Autonomous.kPYController);
-        SmartDashboard.setDefaultNumber("P Gain Auto thetaCotroller", Constants.Autonomous.kPThetaController);
-        SmartDashboard.setDefaultNumber("Auto Max Velocity", Constants.Autonomous.kMaxSpeedMetersPerSecond);
-        SmartDashboard.setDefaultNumber("Auto Max Accleration",
-                Constants.Autonomous.kMaxAccelerationMetersPerSecondSquared);
-
-        SmartDashboard.putData("Update Auto PID", new InstantCommand(() -> updatePID()));
-        SmartDashboard.putData("Update Auto Trajectory", new InstantCommand(() -> updateTrajectory()));
-
-        this._chooser = new SendableChooser<String>();
-        _chooser.setDefaultOption("straight path", "straight_path");
-        _chooser.addOption("s path", "s");
-        _chooser.addOption("180 rotation path", "rotate");
-        _chooser.addOption("collect 1 CARGO", "collect");
-        // Add a new autonomous path:
-        // _chooser.addOption("straight path", "straight_path");
-
-        SmartDashboard.putData("paths chooser", this._chooser);
-    }
-
-    public void periodic() {
-        SmartDashboard.putNumber("x pose", m_drivetrain.getPose().getX());
-    }
-
-    private void updatePID() {
-        _xController.setP(SmartDashboard.getNumber("P Gain Auto xController", Constants.Autonomous.kPXController));
-        _yController.setP(SmartDashboard.getNumber("P Gain Auto yController", Constants.Autonomous.kPYController));
-        _thetaController
-                .setP(SmartDashboard.getNumber("P Gain Auto thetaCotroller", Constants.Autonomous.kPThetaController));
-    }
-
-    private void updateTrajectory() {
-        this.m_trajectory = PathPlanner.loadPath(this._chooser.getSelected(),
-                SmartDashboard.getNumber("Auto max velocity", Constants.Autonomous.kMaxSpeedMetersPerSecond),
-                SmartDashboard.getNumber("Auto Max Accleration",
-                        Constants.Autonomous.kMaxAccelerationMetersPerSecondSquared));
     }
 
     public Command getFollowTrajectoryCommand() {
