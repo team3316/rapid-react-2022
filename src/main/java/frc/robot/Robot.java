b7b46b6a11ba20bcb30465790c58114a7a35d3c3
Mika <mikush.carmel@gmail.com>
2022-03-13 13:24:15 +0200

    added LEDs for timer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.LED.RobotColorState;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -48,6 +49,14 @@ public class Robot extends TimedRobot {
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
+
+    if(DriverStation.getMatchTime() > 0.0){
+        if(DriverStation.getMatchTime() < 5.0)
+            m_robotContainer.setBlinkLEDs(RobotColorState.FIVE_SEC);
+        else if(DriverStation.getMatchTime() < 15.0)
+            m_robotContainer.setBlinkLEDs(RobotColorState.FIFTEEN_SEC);
+    }
+
     SmartDashboard.putNumber("time", DriverStation.getMatchTime());
     CommandScheduler.getInstance().run();
   }
