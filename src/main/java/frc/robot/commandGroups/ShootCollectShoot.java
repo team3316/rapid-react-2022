c229adfba590d4bb6bca15b7f56fbfcc216c5674
Mika <mikush.carmel@gmail.com>
2022-03-13 18:04:38 +0200

    removed overload for arm goal command

--- src/main/java/frc/robot/commandGroups/ShootCollectShoot.java
+++ src/main/java/frc/robot/commandGroups/ShootCollectShoot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Constants;
 import frc.robot.autonomous.FollowTrajectory;
 import frc.robot.commands.Collect;
+import frc.robot.subsystems.LED;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.manipulator.Manipulator;
@@ -17,18 +18,18 @@ import frc.robot.subsystems.trigger.Trigger;
 
 public class ShootCollectShoot extends SequentialCommandGroup {
 
-    public ShootCollectShoot(Drivetrain drivetrain, Manipulator manipulator, Trigger trigger, Arm arm) {
+    public ShootCollectShoot(Drivetrain drivetrain, Manipulator manipulator, Trigger trigger, Arm arm, LED led) {
 
         FollowTrajectory followTrajectory = new FollowTrajectory(drivetrain, "collect");
         FollowTrajectory followTrajectoryReversed = new FollowTrajectory(drivetrain, "collect_rev");
 
         addCommands(
-                new AutonomousShoot(arm, manipulator, trigger),
-                new InstantCommand(() -> arm.getActiveGoalCommand(Constants.ArmConstants.intakeAngle).schedule()),
+                new AutonomousShoot(arm, manipulator, trigger, led),
+                new InstantCommand(() -> arm.getActiveGoalCommand(Constants.ArmConstants.intakeAngle, led).schedule()),
                 new WaitUntilCommand(arm::atGoal),
                 followTrajectory.getResetOddometryCommand(),
                 followTrajectory.getFollowTrajectoryCommand().deadlineWith(new Collect(manipulator)),
                 followTrajectoryReversed.getFollowTrajectoryCommand(),
-                new AutonomousShoot(arm, manipulator, trigger));
+                new AutonomousShoot(arm, manipulator, trigger, led));
     }
 }
