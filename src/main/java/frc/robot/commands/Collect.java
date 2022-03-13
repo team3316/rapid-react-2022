ec5ac05e9c65c9054c74d05500139eae6eef9dfd
Mika <mikush.carmel@gmail.com>
2022-03-13 13:23:25 +0200

    added LEDs for collection

--- src/main/java/frc/robot/commands/Collect.java
+++ src/main/java/frc/robot/commands/Collect.java
@@ -5,12 +5,29 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.LED;
+import frc.robot.subsystems.LED.RobotColorState;
 import frc.robot.subsystems.manipulator.Manipulator;
 import frc.robot.subsystems.manipulator.Manipulator.ManipulatorState;
+import frc.robot.utils.LatchedBoolean;
 
 public class Collect extends CommandBase {
 
     private Manipulator _manipulator;
+
+    private LatchedBoolean _cargoState;
+
+    private LED _led;
+
+    public Collect(Manipulator manipulator, LED led) {
+
+        this._manipulator = manipulator;
+        addRequirements(this._manipulator);
+
+        this._led = led;
+        this._cargoState = new LatchedBoolean();
+        this._cargoState.update(false);
+    }
 
     public Collect(Manipulator manipulator) {
 
@@ -22,17 +39,27 @@ public class Collect extends CommandBase {
     @Override
     public void initialize() {
         this._manipulator.setState(ManipulatorState.COLLECT);
+        this._led.setRobotLEDs(RobotColorState.COLLECT);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        if(_cargoState.update(this._manipulator.getCargoState().hasOne())){
+            this._led.setRobotLEDs(RobotColorState.ONE_CARGO);
+        }
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
         this._manipulator.setState(ManipulatorState.OFF);
+        if(interrupted){
+            this._led.setRobotLEDs(RobotColorState.DEFAULT);
+        }
+        else{
+            this._led.setRobotLEDs(RobotColorState.TWO_CARGO);
+        }
     }
 
     // Returns true when the command should end.
