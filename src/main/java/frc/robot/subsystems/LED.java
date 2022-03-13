44b6cb39438b5403ca9fcfdc2fa4f120e4554350
Mika <mikush.carmel@gmail.com>
2022-03-13 18:11:29 +0200

    changed blinking + setLED method

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -7,93 +7,73 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.LED.RobotColorState;
 
 public class LED extends SubsystemBase {
     private AddressableLED _led;
-    private AddressableLEDBuffer _buffer;
-    private Blinker _blinker;
+    private AddressableLEDBuffer _mainBuffer, _blinkBuffer;
 
-    private class Blinker {
-        private double _lastChange;
-        private boolean _on;
+    private boolean _isBlinking = false;
 
-        private final Color _onColor;
-        private final Color _offColor;
-        private final double _interval;
-        
+    private double _lastChange;
+    private boolean _on;
 
-        public Blinker(double interval, Color onColor, Color offColor) {
-            _interval = interval;
-            _onColor = onColor;
-            _offColor = offColor;
+    private final RobotColorState _onState;
+    private final RobotColorState _offState;
+    private final double _interval;
+
+    public LED() {
+        this._led = new AddressableLED(Constants.LED.port);
+        this._mainBuffer = new AddressableLEDBuffer(Constants.LED.length);
+        this._blinkBuffer = new AddressableLEDBuffer(Constants.LED.length);
+
+        this._led.setLength(this._mainBuffer.getLength());
+
+        this._led.setData(this._mainBuffer);
+        this._led.start();
+
+        this._lastChange = 0.0;
+        this._interval = 0.0;
+
+        this._onState = null;
+        this._offState = null;
+
+        this._on = false;
+    }
+
+    public void setLED(RobotColorState state) {
+        for (int i = 0; i < this._mainBuffer.getLength(); i++) {
+            this._mainBuffer.setLED(i, state.color);
         }
 
-        public void update() {
+        this._led.setData(this._mainBuffer);
+    }
+
+    public void setBlink(RobotColorState state) {
+        this._isBlinking = true;
+
+        for(int i = 0; i < this._blinkBuffer.getLength(); i++){
+            this._blinkBuffer.setLED(i, state.color);
+        }
+
+        this._led.setData(this._blinkBuffer);
+    }
+
+    @Override
+    public void periodic() {
+        if(this._isBlinking){
             double timestamp = Timer.getFPGATimestamp();
             if (timestamp - _lastChange > _interval){
                 _on = !_on;
                 _lastChange = timestamp;
                 if (_on){
-                    setLED(_onColor);
+                    setLED(this._onState);
                 } else {
-                    setLED(_offColor);
+                    setLED(this._offState);
                 }
             } 
         }
-        
-    }
-
-    public LED() {
-        this._led = new AddressableLED(Constants.LED.port);
-        this._buffer = new AddressableLEDBuffer(Constants.LED.length);
-
-        this._led.setLength(this._buffer.getLength());
-
-        this._led.setData(this._buffer);
-        this._led.start();
-    }
-
-    private void setLED(Color color) {
-        _blinker = null;
-        for (int i = 0; i < this._buffer.getLength(); i++) {
-            this._buffer.setLED(i, color);
-        }
-
-        this._led.setData(this._buffer);
-    }
-
-    public void setBlink(RobotColorState state) {
-
-        _blinker = new Blinker(1, state.color, new Color(0,0,0));
-        this._led.stop();
-    }
-
-    public enum RobotColorState {
-        ONE_CARGO(Constants.LED.oneCargoColor), // collect one CARGO
-        TWO_CARGO(Constants.LED.bothCargoColor), // collect two CARGO
-        COLLECT(Constants.LED.noneCargoColor), // defualt color for collecting (if there aren't CARGOs)
-        ARM_UP(Constants.LED.armUpColor), // arm in shooting position
-        FIFTEEN_SEC(Constants.LED.fifteenSecColor), // fifteen seconds left until the match ends
-        FIVE_SEC(Constants.LED.fiveSecColor), // five seconds left until the match ends
-        DEFAULT(Constants.LED.defaultCargoColor); // defualt color
-
-        public Color color;
-
-        private RobotColorState(Color color) {
-            this.color = color;
-        }
-    }
-
-    public void setRobotLEDs(RobotColorState state) {
-        setLED(state.color);
-    }
-
-    @Override
-    public void periodic() {
-        if(_blinker != null) 
-            _blinker.update();
     }
 }
