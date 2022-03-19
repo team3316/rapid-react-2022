bfb9f96444df0226eb600eb05d4403536e5375ca
Mika <mikush.carmel@gmail.com>
2022-03-19 18:44:14 +0200

    changed colors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -74,7 +74,9 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+      m_robotContainer.rainBow();
+  }
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override 
